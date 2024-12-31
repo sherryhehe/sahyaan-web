45cd6b4f859934d71f263c6e1cc30675af9d1982
Muhammad Humam <humamchoudhary@gmail.com>
2025-01-01 00:04:45 +0500

    update

--- src/app/api/checkout-session/route.js
+++ src/app/api/checkout-session/route.js
@@ -1,7 +1,7 @@
 // app/api/checkout-session/route.js
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {});
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
